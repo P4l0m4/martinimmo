1925c3b92306e2f468cce20d29a2d31f33735e35
Paloma Tejeda <palomatejeda81@gmail.com>
2024-09-25 17:18:16 +0200

    footer update

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -6,46 +6,78 @@ const [showSignIn, toggleSignIn] = useToggle();
 </script>
 <template>
   <footer>
-    <ul class="links">
-      <li class="links__li"><NuxtLink to="/">Accueil</NuxtLink></li>
-      <li class="links__li"><NuxtLink to="/recherche">Recherche</NuxtLink></li>
-      <li class="links__li">
-        <NuxtLink to="/mon-compte">Mon compte</NuxtLink>
-      </li>
-    </ul>
-    <ul class="links">
-      <li class="links__li">
-        <NuxtLink to="tekilawebfactory.com"
-          >Made By Tekila Web Factory</NuxtLink
+    <GridContainer>
+      <div class="title-section">
+        <img
+          class="title-section__logo"
+          src="@/assets/images/immo-logo.svg"
+          alt="logo martinimmo"
+        />
+        <h4 class="title-section__title">Martinimmo</h4>
+      </div>
+      <ul class="links">
+        <h5 class="links__title">Liens utiles</h5>
+        <li class="links__li"><NuxtLink to="/">Accueil</NuxtLink></li>
+        <li class="links__li">
+          <NuxtLink to="/recherche">Recherche</NuxtLink>
+        </li>
+        <li class="links__li">
+          <NuxtLink to="/mon-compte">Mon compte</NuxtLink>
+        </li>
+      </ul>
+      <ul class="links">
+        <h5 class="links__title">A propos</h5>
+        <li class="links__li">
+          <NuxtLink to="tekilawebfactory.com"
+            >Made By Tekila Web Factory</NuxtLink
+          >
+        </li>
+        <li class="links__li">508 rue Pichat</li>
+        <li class="links__li">73800, Ste-Hélène-du-Lac</li>
+      </ul>
+      <div class="buttons">
+        <PrimaryButton button-type="light" @click="toggleSignUp"
+          >Inscription</PrimaryButton
         >
-      </li>
-      <li class="links__li">
-        <button class="button--tertiary-light">Mentions légales</button>
-      </li>
-      <PrimaryButton button-type="light" @click="toggleSignUp"
-        >Inscription</PrimaryButton
-      >
-      <SecondaryButton button-type="light" @click="toggleSignIn"
-        >Connexion</SecondaryButton
-      >
+        <SecondaryButton button-type="light" @click="toggleSignIn"
+          >Connexion</SecondaryButton
+        >
 
-      <SignUp v-if="showSignUp" @close-sign-up="showSignUp = !showSignUp" />
-      <SignIn v-if="showSignIn" @close-sign-in="showSignIn = !showSignIn" />
-    </ul>
+        <SignUp v-if="showSignUp" @close-sign-up="showSignUp = !showSignUp" />
+        <SignIn v-if="showSignIn" @close-sign-in="showSignIn = !showSignIn" />
+      </div>
+    </GridContainer>
+    <div class="bottom-links">
+      <NuxtLink to="tekilawebfactory.com">CGV</NuxtLink
+      ><NuxtLink to="tekilawebfactory.com">RGPD</NuxtLink
+      ><NuxtLink to="tekilawebfactory.com">Mentions légales</NuxtLink>
+      <NuxtLink to="tekilawebfactory.com">©Tekila Web Factory 2025</NuxtLink>
+    </div>
   </footer>
 </template>
 <style lang="scss" scoped>
 footer {
   background-color: $secondary-color;
   padding: 1rem;
+  gap: 2rem;
   display: flex;
   flex-direction: column;
-  gap: 2rem;
 
-  @media (min-width: $big-tablet-screen) {
-    flex-direction: row;
-    padding: 2rem;
-    gap: 4rem;
+  .title-section {
+    display: flex;
+    gap: 0.5rem;
+    height: fit-content;
+    align-items: center;
+
+    &__logo {
+      width: 40px;
+      height: 40px;
+    }
+
+    &__title {
+      color: $text-color-alt;
+      font-size: $titles;
+    }
   }
 
   .links {
@@ -54,10 +86,42 @@ footer {
     list-style: none;
     flex-direction: column;
 
+    &__title {
+      color: $text-color-alt;
+      font-size: $subtitles;
+    }
+
     &__li {
+      color: $text-color-alt;
       a {
         color: $text-color-alt;
       }
+    }
+  }
+
+  .buttons {
+    display: flex;
+    gap: 1rem;
+    height: fit-content;
+    flex-direction: column;
+
+    @media (min-width: $big-tablet-screen) {
+      flex-direction: row;
+    }
+  }
+
+  .bottom-links {
+    display: flex;
+    gap: 1rem;
+    color: $primary-color-faded;
+    font-size: $small-text;
+    border-top: 1px solid $primary-color-faded;
+    width: 100%;
+    padding-top: 2rem;
+
+    & a {
+      color: $primary-color-faded;
+      white-space: nowrap;
     }
   }
 }
