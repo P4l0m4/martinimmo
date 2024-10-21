e9eb607ae02a883728dd56811aa42b75005c7cd8
Paloma Tejeda <palomatejeda81@gmail.com>
2024-10-21 17:22:10 +0200

    blog makeover

--- src/pages/blog/[slug].vue
+++ src/pages/blog/[slug].vue
@@ -37,9 +37,8 @@ onMounted(async () => {
 
     <div class="article-content" v-html="renderRichText(article?.text)"></div>
   </Container>
-  <Container>
-    <LastArticles />
-  </Container>
+
+  <LastArticles />
 </template>
 <style scoped lang="scss">
 .article-preview-image {
