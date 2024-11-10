bae40e0e65b6f0b703216e60439cbbd94dc44ae6
Paloma Tejeda <palomatejeda81@gmail.com>
2024-11-10 18:34:05 +0100

    password recovery update

--- src/pages/auth-reset.vue
+++ src/pages/auth-reset.vue
@@ -1,18 +1,80 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { useRouter } from "vue-router";
-import {
-  updateUserPassword,
-  signOut,
-  checkExistingToken,
-} from "@/utils/supabase";
+import { updateUserPassword, checkExistingToken } from "@/utils/supabase";
+import { useVuelidate } from "@vuelidate/core";
+import { required, email, sameAs, minLength } from "@vuelidate/validators";
 
 const router = useRouter();
 const newPassword = ref("");
 const isUserLoggedIn = await checkExistingToken();
 const mail = isUserLoggedIn.user.email;
 const buttonState = ref<"default" | "success" | "error" | "loading">("default");
+const buttonLabel = computed(() => {
+  if (passwordErrors.value.length > 0) {
+    return passwordErrors.value[0];
+  } else {
+    return "Mettre à jour";
+  }
+});
+const uppercaseRegex = /[A-Z]/;
+const hasUpperCase = (value: string) => uppercaseRegex.test(value);
+
+const lowercaseRegex = /[a-z]/;
+const hasLowerCase = (value: string) => lowercaseRegex.test(value);
+
+const numberRegex = /[0-9]/;
+const hasNumber = (value: string) => numberRegex.test(value);
+
+const specialCharRegex = /[!@#$%^&*(),.?":{}|<>]/;
+const hasSpecialChar = (value: string) => specialCharRegex.test(value);
+
+const passwordComplexity = {
+  required,
+  minLength: minLength(8),
+  hasUpperCase,
+  hasLowerCase,
+  hasNumber,
+  hasSpecialChar,
+};
+const rules = {
+  password: passwordComplexity,
+};
+const state = {
+  password: newPassword.value,
+};
+
+const v$ = useVuelidate(rules, state);
+const passwordErrors = computed(() => {
+  const errors: string[] = [];
+  if (!v$.value.password.$dirty) return errors;
+  if (v$.value.password.required.$invalid) {
+    errors.push("Mot de passe vide");
+  }
+  if (v$.value.password.minLength.$invalid) {
+    errors.push("Au moins 8 caractères");
+  }
+  if (v$.value.password.hasUpperCase.$invalid) {
+    errors.push("Au moins une majuscule");
+  }
+  if (v$.value.password.hasLowerCase.$invalid) {
+    errors.push("Au moins une minuscule");
+  }
+  if (v$.value.password.hasNumber.$invalid) {
+    errors.push("Au moins un chiffre");
+  }
+  if (v$.value.password.hasSpecialChar.$invalid) {
+    errors.push("Au moins un caractère spécial");
+  }
+  return errors;
+});
+
 const resetPassword = async () => {
+  v$.value.$touch();
+  if (v$.value.$invalid) {
+    buttonState.value = "error";
+    return;
+  }
   const { success } = await updateUserPassword(mail, newPassword.value);
 
   if (success) {
@@ -27,7 +89,6 @@ const resetPassword = async () => {
 <template>
   <Container>
     <h2>Votre nouveau mot de passe</h2>
-    <p>{{ mail }}</p>
     <form class="form" @submit.prevent="resetPassword">
       <InputField
         type="password"
@@ -37,6 +98,7 @@ const resetPassword = async () => {
         placeholder="!Vq0h2@8RHi"
         label="Nouveau mot de passe"
         required
+        :error="passwordErrors[0]"
       />
 
       <PrimaryButton
@@ -44,7 +106,7 @@ const resetPassword = async () => {
         :button-state="buttonState"
         type="submit"
       >
-        Update Password
+        {{ buttonLabel }}
       </PrimaryButton>
     </form>
   </Container>
